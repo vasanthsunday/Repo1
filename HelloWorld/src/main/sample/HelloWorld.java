bb1fa8763cb7810e5b57c2653bce4924ac7da890
vasanth <vasanth@localhost>
2020-05-09 00:50:47 +0530

    just commit

---
+++ HelloWorld/src/main/sample/HelloWorld.java
@@ -0,0 +1,14 @@
+package main.sample;
+
+public class HelloWorld {
+
+	public HelloWorld() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.print("vasanth");
+	}
+
+}
